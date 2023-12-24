748e264c26a3d9cfff1184c5fba8141c3d45f324
Not-element <147367688+Not-element@users.noreply.github.com>
2023-12-24 20:18:34 +0800

    Delete onlineshop directory

--- onlineshop/buyer/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from . import models
-
-
-class BuyerInfoForm(forms.ModelForm):
-
-    class Meta:
-        model = models.BuyerInfo
-        fields = ['buyer_name', 'buyer_image', 'password', 'email']
--- onlineshop/buyer/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class BuyerInfo(models.Model):
-    buyer_name = models.CharField(max_length=15)
-    buyer_image = models.ImageField(upload_to='user', default='/user/4357443aa53522b.jpg', verbose_name='用户头像')
-    password = models.CharField(max_length=11)
-    email = models.CharField(max_length=25, default='666@qq.com')
